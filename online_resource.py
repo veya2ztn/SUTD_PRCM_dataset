8feed37c424cb996483284093c783d33ad49b338
veya2ztn <zhangtianning110@gmail.com>
2021-05-13 00:04:27 +0800

    good image unicode

---
+++ online_resource.py
@@ -0,0 +1,18 @@
+online_path={"PLG.B1NES32"  :"https://www.dropbox.com/s/zf88vvtrxcfwfdp/B1NES32.zip?dl=1",
+             "PLG.B1NES128" :"https://www.dropbox.com/s/oibyqzbgoxtrzic/B1NES128.zip?dl=1",
+             "PLG.B1NES1001":"https://www.dropbox.com/s/c49y6d3opn5ypsd/B1NES1001.zip?dl=1",
+            "PLG250.B1NES32":"https://www.dropbox.com/s/8jgtnj10h8t53kj/B1NES32.751to1001.zip?dl=1",
+           "PLG250.B1NES128":"https://www.dropbox.com/s/p4ctaz6zkywu8gx/B1NES128.751to1001.zip?dl=1",
+
+             "PTN.B1NES32"  :"https://www.dropbox.com/s/m94yf9nanh7rbjr/B1NES32.zip?dl=1",
+             "PTN.B1NES128" :"https://www.dropbox.com/s/7i37wxad3n67ixa/B1NES128.zip?dl=1",
+             "PTN.B1NES1001":"https://www.dropbox.com/s/tb6fnsbo6wajh6b/B1NES1001.zip?dl=1",
+
+             "PLR.B1NES32"  :"https://www.dropbox.com/s/tw8idvltojk6mig/B1NES32.zip?dl=1",
+             "PLR.B1NES128" :"https://www.dropbox.com/s/mnhbit9owkcsxxe/B1NES128.zip?dl=1",
+             "PLR.B1NES1001":"https://www.dropbox.com/s/rzgwdn5nlibv04j/B1NES1001.zip?dl=1",
+
+             "RDN.B1NES32"  :"https://www.dropbox.com/s/yq95z1j15ae3356/B1NES32.zip?dl=1",
+             "RDN.B1NES128" :"https://www.dropbox.com/s/85yduq6zu07pwk5/B1NES128.zip?dl=1",
+             "RDN.B1NES1001":"https://www.dropbox.com/s/53t2qdzgwvel6kt/B1NES1001.zip?dl=1",
+}
